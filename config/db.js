b9221d6982ef0ea5b5062fc679684f9fee22bf38
SHR <t41296193@gmail.com>
2025-01-13 18:02:30 +0600

    bd

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ import mongoose  from 'mongoose';
 const connectionDB = async() => {
     try {
         await mongoose.connect(process.env.URL);
-         console.log('database is runing....'); 
+         console.log('database is runing....userName:t41296193: pasword:29Kwrj7giXMDPCpi'); 
     } catch (error) {
         console.log('database' + error);
         
